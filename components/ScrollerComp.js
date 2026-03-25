a21ba71970401e42f63f57b720e32270d887e609
KavitaR45 <kavitarawat1214@gmail.com>
2026-03-25 17:38:19 +0530

    added bold fonts

--- components/ScrollerComp.js
+++ components/ScrollerComp.js
@@ -1,7 +1,7 @@
 import Marquee from "react-fast-marquee";
 
 const ScrollerComp = ({ children, speed = 50, direction = "left" }) => (
-  <Marquee
+  <Marquee style={{gap:"50px"}}
     speed={speed} 
     direction={direction} // "left" | "right"
     autoFill
